522940c056b19c1138305df1e4b8b7fbe5d58d5c
Raghu <raghu@raghus-MacBook-Pro.local>
2017-08-26 02:04:21 -0700

    Adding program: reverse words in a sentence

---
+++ src/strings/ReverseWordsInSentence/ReverseWordsInSentence.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+void ReverseWordsInSentence(string& sentence)
+{
+    if (sentence.empty()) 
+    {
+        return;
+    }
+
+    reverse(sentence.begin(), sentence.end());
+    size_t wStart = 0, wEnd = 0;
+    size_t len = sentence.size();
+    
+    for (wEnd = 0; wEnd < len; ++wEnd) 
+    {
+        if (sentence[wEnd] != ' ') 
+        {
+            wStart = wEnd;
+            while (wEnd < len && sentence[wEnd] != ' ') 
+            {
+                ++wEnd;
+            }
+            reverse(sentence.begin() + wStart, sentence.begin() + wEnd);
+        }
+    }
+}
+// Driver program to test above functions
+int main()
+{
+    string values[] = { "a ",
+                        "a b ",
+                        "aba bab c",
+                        "This is Phoenix string",
+    };
+    for (auto sentence : values) 
+    {
+        cout << sentence << endl;
+        ReverseWordsInSentence(sentence);
+        cout << sentence << endl;
+    }
+}
