788d89adfce4c4f8dc5c0bfb48fa2253213d0fc6
Raghu Ramaswamy <raghura@windows.microsoft.com>
2017-09-18 13:17:41 -0700

    Perfect rectangle, code update

--- src/Contest_problems/PerfectRectangle/PerfectRectangle.cpp
+++ src/Contest_problems/PerfectRectangle/PerfectRectangle.cpp
@@ -1,6 +1,3 @@
-// StringPermutations.cpp : Defines the entry point for the console application.
-//
-
 #include <iostream>
 #include <vector>
 #include <string>
@@ -9,87 +6,106 @@ using namespace std;
 
 // https://leetcode.com/contest/leetcode-weekly-contest-2/problems/perfect-rectangle/
 
-bool isRectangleCover(vector<vector<int>>& rectangles) 
+bool RectangleCover(const vector<vector<size_t>>& rectangles)
 {
-    bool flag = 0;
-    bool temp_flag = 1;
-    int count = rectangles.size();
-    int total_area = 0;
-    int temp_area = 0;
-    int left_x = rectangles[0][0], left_y = rectangles[0][1];
-    int right_x = rectangles[0][2], right_y = rectangles[0][3];
+	bool isRectanglePerfect = false;
+	bool isRectangleOverlap = false;
+	size_t totalArea = 0;
+	size_t tempArea = 0;
+	size_t left_x = rectangles[0][0], left_y = rectangles[0][1];
+	size_t right_x = rectangles[0][2], right_y = rectangles[0][3];
 
-    if (count == 1)
-    {
-        return true;
-    }
+	if (rectangles.size() == 1)
+	{
+		return true;
+	}
 
-    for (int i = 0; i < count; i++)
-    {
-        if (temp_flag)
-        {
-        for (int j = 0; j < count; j++)
-        {
-            if (i != j)
-            {
-                if ((rectangles[j][0] < rectangles[i][2] && rectangles[i][2] <= rectangles[j][2]) 
-                && 
-                (rectangles[j][1] < rectangles[i][3] && rectangles[i][3] <= rectangles[j][3]))
-                {
-                    temp_flag = 0;
-                }
-            }
+	for (size_t i = 0; i < rectangles.size(); i++)
+	{
+		if (!isRectangleOverlap)
+		{
+			for (size_t j = i+1; j < rectangles.size(); j++)
+			{
+				if ((rectangles[j][0] < rectangles[i][2] && rectangles[i][2] <= rectangles[j][2])&&
+					(rectangles[j][1] < rectangles[i][3] && rectangles[i][3] <= rectangles[j][3]))
+				{
+					isRectangleOverlap = true;
+				}
+			}
 
-        }
+			tempArea += (rectangles[i][2] - rectangles[i][0])*(rectangles[i][3] - rectangles[i][1]);
+			if (rectangles[i][0] < left_x)
+			{
+				left_x = rectangles[i][0];
+			}
+			if (rectangles[i][1] < left_y)
+			{
+				left_y = rectangles[i][1];
+			}
+			if (rectangles[i][2] > right_x)
+			{
+				right_x = rectangles[i][2];
+			}
+			if (rectangles[i][3] > right_y)
+			{
+				right_y = rectangles[i][3];
+			}
+		}
+	}
+	totalArea = (right_x - left_x)*(right_y - left_y);
 
-        temp_area += (rectangles[i][2] - rectangles[i][0])*(rectangles[i][3] - rectangles[i][1]);
-        if (rectangles[i][0] < left_x)
-        {
-            left_x = rectangles[i][0];
-        }
-        if (rectangles[i][1] < left_y)
-        {
-            left_y = rectangles[i][1];
-        }
-        if (rectangles[i][2] > right_x)
-        {
-            right_x = rectangles[i][2];
-        }
-        if (rectangles[i][3] > right_y) 
-        {
-            right_y = rectangles[i][3];
-        }
-        
-        //else right_y = rectangles[i + 1][3];
-        }
-    }
-    //temp_area += (rectangles[count-1][2] - rectangles[count-1][0])*(rectangles[count-1][3] - rectangles[count-1][1]);
-    total_area = (right_x - left_x)*(right_y - left_y);
+	if (totalArea == tempArea && !isRectangleOverlap)
+	{
+		isRectanglePerfect = true;
+	}
 
-    if (total_area == temp_area && temp_flag == 1) 
-        flag = 1;
-    return flag;
+	return isRectanglePerfect;
 }
 
-int main()
+size_t main()
 {
-    vector<vector<int>> rectangles = {
-        {1,1,3,3},
-        {3,1,4,2},
-        {3,2,4,4},
-        {1,3,2,4},
-        {2,3,3,4},
-    };
+	vector<vector<size_t>> rectangles = {
+		// perfect rectangle
+		{ 1,1,3,3 },
+		{ 3,1,4,2 },
+		{ 3,2,4,4 },
+		{ 1,3,2,4 },
+		{ 2,3,3,4 },
+		
 
-    bool res = isRectangleCover(rectangles);
-    if (res)
-    {
-        cout << "Yes" << endl;
-    }
-    else
-    {
-        cout << "No" << endl;
-    }
-    return 0;
+		// Not complete, gap in between case 1
+		/*
+		{ 1,1,2,3 },
+		{ 1,3,2,4 },
+		{ 3,1,4,2 },
+		{ 3,2,4,4 },
+		*/
+
+		// Not complete, gap in between case 2
+		/*
+		{ 1,1,3,3 },
+		{ 3,1,4,2 },
+		{ 1,3,2,4 },
+		{ 3,2,4,4 },
+		*/
+
+		// Complete, but overlap in rectangles
+		/*
+		{ 1,1,3,3 },
+		{ 3,1,4,2 },
+		{ 1,3,2,4 },
+		{ 2,2,4,4 },
+		*/
+	};
+
+	bool res = RectangleCover(rectangles);
+	if (res)
+	{
+		cout << "Yes" << endl;
+	}
+	else
+	{
+		cout << "No" << endl;
+	}
+	return 0;
 }
-
