cb7e13aab2ed86fdbae721f132b41efdf4d30db4
Raghu <raghu@raghus-MacBook-Pro.local>
2017-10-06 18:18:02 -0700

    Chunked Palindrome

---
+++ src/strings/ChunkedPalindrome/ChunkedPalindrome.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+size_t _ChunkedPalindrome(const string& str, size_t& low, size_t& high)
+{
+    if (str.empty() || low > high)
+    {
+        return 0;
+    }
+    else if (low == high)
+    {
+        return 1;
+    }
+    else if (low == high-1)
+    {
+        return str[low] == str[high] ? 2 : 1;
+    }
+
+    bool canChunkWord = false;
+    size_t start = low;
+    size_t end = high;
+    while (low < high)
+    {
+        const string& s1 = str.substr(start, low - start+1);
+        const string& s2 = str.substr(high, end - high+1);
+        if (!s1.empty() && !s2.empty() &&s1 == s2)
+        {
+            canChunkWord = true;
+            low++;
+            high--;
+            return _ChunkedPalindrome(str, low, high) + 2;
+        }
+        else
+        {
+            low++;
+            high--;    
+        }
+    }
+    if (!canChunkWord)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+size_t ChunkedPalindrome(const string& word)
+{
+    if (word.empty())
+    {
+        return 0;
+    }
+    
+    size_t low = 0, high = word.size()-1;
+    return _ChunkedPalindrome(word, low, high);
+}
+
+int main()
+{
+    const vector<string> inputs = {
+        "ghiabcdefhelloadamhelloabcdefghi",
+        "abccab",
+        "a",
+        "ab",
+        "aa",
+        "merchant",
+        "antaprezatepzapreanta",
+        "geeksforgeeks",
+        "randomrandom"
+    };
+    cout << endl << "Chunked palindromes: " << endl;
+    for (const auto& word : inputs)
+    {
+        size_t count = ChunkedPalindrome(word);
+        cout << word << ": " << count << endl;
+    }
+    cout << endl;
+    return 0;
+}
