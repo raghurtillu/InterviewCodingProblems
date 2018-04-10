39a115962ded852e6db4e225b57e96d5388b97e2
Raghu <raghu@raghus-MacBook-Pro.local>
2018-04-10 15:49:32 -0700

    Is interleave cleaned up code

--- src/strings/IsInterleave/IsInterleave.cpp
+++ src/strings/IsInterleave/IsInterleave.cpp
@@ -8,32 +8,56 @@ using namespace std;
 // c is said to be interleave of a and b, if it contains all characters of a and b and order 
 // of all characters in the individual strings is preserved
 
-bool dfs(const string& s1, const string& s2, const string& s3,
-    size_t i, size_t j, size_t k, unordered_map<size_t, size_t>& lookup)
+struct Element
 {
-    if (lookup.find(i) != lookup.cend() && lookup[i] == j) 
+    size_t x = 0, y = 0, z = 0;
+
+    Element(size_t _x, size_t _y, size_t _z) : x(_x), y(_y), z(_z)
+    {}
+    Element() = default;
+    Element& operator= (const Element& ) = default;
+    bool operator== (const Element &rhs) const
+    {
+        return x == rhs.x && y == rhs.y && z == rhs.z;
+    }
+};
+struct ElementHash
+{
+    size_t operator()(const Element& key) const
+    {
+        return std::hash<size_t>()(key.x) ^ std::hash<size_t>()(key.y) ^ std::hash<size_t>()(key.z);
+    }
+};
+
+bool _isInterleave(const string& s1, const string& s2, const string& s3,
+    size_t i, size_t j, size_t k, 
+    unordered_map<Element, bool, ElementHash>& lookup)
+{
+    if (i > s1.size() || j > s2.size() || k > s3.size())
     {
         return false;
     }
-    else if (k >= s3.size())
+    else if (i == s1.size() && j == s2.size() && k == s3.size())
     {
         return true;
     }
+    else if (lookup.find(Element(i, j, k)) != lookup.cend())
+    {
+        return lookup[Element(i, j, k)];
+    }
 
+    Element e(i, j, k);
     bool res = false;
     if (i < s1.size() && s1[i] == s3[k])
     {
-        res = dfs(s1, s2, s3, i + 1, j, k + 1, lookup);
+        res = _isInterleave(s1, s2, s3, i + 1, j, k + 1, lookup);
     }
     if (!res && j < s2.size() && s2[j] == s3[k])
     {
-        res = dfs(s1, s2, s3, i, j + 1, k + 1, lookup);
+        res = _isInterleave(s1, s2, s3, i, j + 1, k + 1, lookup);
     }
-    if (!res)
-    {
-        lookup[i] = j;
-    }
-    return res;
+    lookup[e] = res;
+    return lookup[e];
 }
 
 bool isInterleave(const string& s1, const string& s2, const string& s3)
@@ -42,9 +66,9 @@ bool isInterleave(const string& s1, const string& s2, const string& s3)
     {
         return false;
     }
-    size_t i = 0, j = 0, k = 0;  
-    unordered_map<size_t, size_t> lookup;
-    return dfs(s1, s2, s3, i, j, k, lookup);
+    size_t i = 0, j = 0, k = 0;
+    unordered_map<Element, bool, ElementHash> lookup;
+    return _isInterleave(s1, s2, s3, i, j, k, lookup);
 }
 int main()
 {
