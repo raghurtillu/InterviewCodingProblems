0192b72d6a32d7ee7ab8abf35bd17c4579de84e3
Raghu <raghu@raghus-MacBook-Pro.local>
2018-07-15 19:46:09 -0700

    Max number, indenting

--- src/stacks/MaxNumber/AdvancedVersion/MaxNumber.cpp
+++ src/stacks/MaxNumber/AdvancedVersion/MaxNumber.cpp
@@ -5,15 +5,20 @@ using namespace std;
 // Given an array of positive numbers and a number k, form maximum number from the input array
 // of lengths [1, k] while maintaining relative order of the numbers in the array
 
-// In the simple idea, we mentioned trying all possible length i. If we create max number for different i from scratch each time, that would be a waste of time. 
-// Suppose num = [4, 9, 3, 2, 1, 8, 7, 6], we need to create max number with length from 1 to 8. For i==8, result is the original array. 
-// For i==7, we need to drop 1 number from array, since 9 > 4, we drop 4, the result is [9, 3, 2, 1, 8, 7, 6]. For i==6, we need to drop 1 more number, 
-// 3 < 9, skip, 2 < 3, skip, 1 < 2, skip, 8 > 1, we drop 1, the result is [9, 3, 2, 8, 7, 6]. For i==5, we need to drop 1 more, but this time, we needn't check from beginning, 
-// during last scan, we already know [9, 3, 2] is monotonically non-increasing, so we check 8 directly, since 8 > 2, we drop 2, the result is [9, 3, 8, 7, 6]. 
-// For i==4, we start with 8, 8 > 3, we drop 3, the result is [9, 8, 7, 6]. For i==3, we start with 8, 8 < 9, skip, 7 < 8, skip, 6 < 7, skip, by now, 
+// In the simple idea, we mentioned trying all possible length i. If we create max number for different i f
+// rom scratch each time, that would be a waste of time. Suppose num = [4, 9, 3, 2, 1, 8, 7, 6], we need to 
+// create max number with length from 1 to 8. For i==8, result is the original array. 
+// For i==7, we need to drop 1 number from array, since 9 > 4, we drop 4, the result is [9, 3, 2, 1, 8, 7, 6]. 
+// For i==6, we need to drop 1 more number, 3 < 9, skip, 2 < 3, skip, 1 < 2, skip, 8 > 1, we drop 1, 
+// the result is [9, 3, 2, 8, 7, 6]. For i==5, we need to drop 1 more, but this time,
+// we needn't check from beginning, during last scan, we already know [9, 3, 2] is monotonically non-increasing,
+// so we check 8 directly, since 8 > 2, we drop 2, the result is [9, 3, 8, 7, 6]. 
+// For i==4, we start with 8, 8 > 3, we drop 3, the result is [9, 8, 7, 6]. 
+// For i==3, we start with 8, 8 < 9, skip, 7 < 8, skip, 6 < 7, skip, by now, 
 // we've got maximum number we can create from num without length constraint. 
 // So from now on, we can drop a number from the end each time. 
-// The result is [9, 8, 7], For i==2, we drop last number 7 and have [9, 8]. For i==1, we drop last number 8 and have [9].
+// The result is [9, 8, 7], For i==2, we drop last number 7 and have [9, 8]. 
+// For i==1, we drop last number 8 and have [9].
 
 void _GetMaxNumber(vector<int> nums, size_t k, vector<vector<int>>& results)
 {
