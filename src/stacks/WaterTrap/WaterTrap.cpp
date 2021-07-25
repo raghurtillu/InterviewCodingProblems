3029b001d37df6279f21aded69507a7418790b19
Raghu <raghu@raghus-MacBook-Pro.local>
2021-07-25 06:15:04 -0700

    WaterTrap code update

--- src/stacks/WaterTrap/WaterTrap.cpp
+++ src/stacks/WaterTrap/WaterTrap.cpp
@@ -6,50 +6,38 @@ using namespace std;
 // Given n non-negative integers representing an elevation map where the width of each bar is 1, 
 // compute how much water it is able to trap after raining.
 // https://leetcode.com/problems/trapping-rain-water/description/
-void GetGreatestHeightLeftandRight(const vector<int>& height, vector<int>& ngeLeft, vector<int>& ngeRight)
+int WaterTrap(const vector<int>& heights)
 {
-    if (height.empty() || ngeLeft.size() != height.size() || ngeRight.size() != height.size()) { return; }
+    if (heights.empty())
+    {
+        return 0;
+    }
     
-    // get greatest height to the left
-    stack<int> lStk;
-    for (size_t i = 0; i < height.size(); ++i)
+    vector<int> ngeLeft(heights.size(), 0), ngeRight(heights.size(), 0);
+    // Get greatest height left
+    int greatestLeft = 0;
+    for (size_t i = 0; i < heights.size(); ++i)
     {
-        while (!lStk.empty() && height[i] >= lStk.top())
-        {
-            lStk.pop();
-        }
-        ngeLeft[i] = !lStk.empty() ? lStk.top() : 0;
-        if (lStk.empty() || height[i] > lStk.top())
-        {
-            lStk.push(height[i]);
-        }
+        ngeLeft[i] = greatestLeft > heights[i] ? greatestLeft : 0;
+        greatestLeft = std::max(greatestLeft, heights[i]);
     }
-    // get greatest height bar to the right
-    stack<int> rStk;
-    for (size_t i = height.size() - 1; i != SIZE_MAX; --i)
-    {
-        while (!rStk.empty() && height[i] >= rStk.top())
-        {
-            rStk.pop();
-        }
-        ngeRight[i] = !rStk.empty() ? rStk.top() : 0;
-        if (rStk.empty() || height[i] > rStk.top())
-        {
-            rStk.push(height[i]);
-        }
-    }
-}
 
-int WaterTrap(const vector<int>& height) 
-{
-    if (height.empty()) { return 0; }
-    vector<int> ngeLeft(height.size()), ngeRight(height.size());
-    GetGreatestHeightLeftandRight(height, ngeLeft, ngeRight);
-    int total = 0;
-    for (size_t i = 0; i < height.size(); ++i)
+    // Get greatest height right
+    int greatestRight = 0;
+    for (size_t i = heights.size() - 1; i != SIZE_MAX; --i)
     {
-        if (ngeLeft[i] == 0 || ngeRight[i] == 0) { continue; }
-        total += min(ngeLeft[i], ngeRight[i]) - height[i];
+        ngeRight[i] = greatestRight > heights[i] ? greatestRight : 0;
+        greatestRight = std::max(greatestRight, heights[i]);
+    }
+    
+    int total = 0;
+    for (size_t i = 0; i < heights.size(); ++i)
+    {
+        if (ngeLeft[i] == 0 || ngeRight[i] == 0)
+        {
+            continue;
+        }
+        total += std::min(ngeLeft[i], ngeRight[i]) - heights[i];
     }
     return total;
 }
